d2e754d5ab7d6af78e4d777643e9d3cbf34aae0c
alvarojbo99 <116230262+alvarojbo99@users.noreply.github.com>
2022-10-20 01:04:25 +0000

    aprendi a mostrar a idade na tela.

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ idade = 23
 #Exibir meu nome (que esta dentro da variavel nome) 
 print(nome)
 
+#Quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável idade 
+#print ("minha idade é" +idade)
+print ("minha idade é" +str(idade))
 
 ''' 
 Comentario em bloco
