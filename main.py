53f5c5d27a7e2f065b41ba9d0e726451723954a1
alvarojbo99 <116230262+alvarojbo99@users.noreply.github.com>
2022-10-20 00:53:41 +0000

    aprendi a criar variáveis e mostra-las na tela.

---
+++ main.py
@@ -0,0 +1,18 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+#Quando quiser guardar uma string! (frase)
+nome = "Alvaro Jesus"
+#Quando quiser guardar um numero inteiro!
+idade = 23 
+
+#Exibir meu nome (que esta dentro da variavel nome) 
+print(nome)
+
+
+''' 
+Comentario em bloco
+
+'''
