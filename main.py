b96ec8613ff858ffdcce217752c7a99f4fa0acf5
alvarojbo99 <116230262+alvarojbo99@users.noreply.github.com>
2022-10-20 01:21:33 +0000

    Aprendemos a demonstrar 3 maneiras diferentes.

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ nome = "Alvaro Jesus"
 #Quando quiser guardar um numero inteiro!
 idade = 23 
 
-#Exibir meu nome (que esta dentro da variavel nome) 
+#Exibir meu nome (que esta dentro da variável nome) 
 print(nome)
 
 #Quando quiser exibir a frase "Minha idade é" completando com o conteúdo da variável idade 
 #print ("minha idade é" +idade)
-print ("minha idade é" +str(idade))
+print ("minha idade é " +str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é ... e tenho ... 
+#anos.." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
 
 ''' 
 Comentario em bloco
