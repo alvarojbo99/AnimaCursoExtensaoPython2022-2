5b4d1e47df52d3bf2b6904369e0d4ef99bade003
alvarojbo99 <116230262+alvarojbo99@users.noreply.github.com>
2022-11-23 23:05:26 +0000

    Aprendi comandos input

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# comando input(): quero permitir que 
+# o usuário digite algo...
+nome = input( "Boa noite! Digite seu nome: ")
 
+idade = int(input("Digite sua idade: "))
 
+#comando de saída..exibir na tela
+print(nome)
+ 
+print("Sua idade é {} anos" .format(idade))
 
+dobro = idade * 2
+
+print("O dobro da idade informada é {} anos" .format(dobro)) 
+
+#Estrutura condicional - o famoso "SE" (if)
+#Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
